a760dd822b14e343b8c2f28afe3aa31562773750
Terin Sebastin <terin01@gmail.com>
2023-03-30 05:11:01 +0530

    first commit

---
+++ AzureFunctionTangyWeb/Models/SalesRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureFunctionTangyWeb.Models
+{
+    public class SalesRequest
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Status { get; set; }
+    }
+}
